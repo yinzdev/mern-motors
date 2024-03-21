4e29b23963360288679b776c04bf3579ec4e41cb
yinzdev <yinzdev@gmail.com>
2024-03-21 15:29:48 -0300

    complete sign up functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,32 +1,76 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>
         Crie sua conta ou inicie sessão
       </h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
           placeholder='Nome'
           className='border p-3 rounded-lg'
           id='username'
+          onChange={handleChange}
         />
         <input
-          type='text'
+          type='email'
           placeholder='E-mail'
           className='border p-3 rounded-lg'
           id='email'
+          onChange={handleChange}
         />
         <input
-          type='text'
+          type='password'
           placeholder='Senha'
           className='border p-3 rounded-lg'
           id='password'
+          onChange={handleChange}
         />
-        <button className='w-60 mx-auto bg-emerald-700 rounded-lg text-white p-3 hover:opacity-90 disabled:opacity-80'>
-          Criar minha conta
+        <button
+          disabled={loading}
+          className='w-60 mx-auto bg-emerald-700 rounded-lg text-white p-3 hover:opacity-90 disabled:opacity-80'
+        >
+          {loading ? 'Carregando...' : 'Criar minha conta'}
         </button>
       </form>
       <div className='flex gap-2 mt-4 justify-center'>
@@ -37,6 +81,7 @@ export default function SignUp() {
           </span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
 }
