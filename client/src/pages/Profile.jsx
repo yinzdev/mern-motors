f3f6afd12bcc54067b1a896c43488e01de1f87c3
yinzdev <yinzdev@gmail.com>
2024-04-09 03:37:49 -0300

    user functionality done

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,17 +7,22 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../firebase';
-
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
 
   // firebase storage
   // allow read
@@ -55,10 +60,37 @@ export default function Profile() {
     );
   };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json ',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Meu perfil</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type='file'
@@ -75,12 +107,12 @@ export default function Profile() {
         <p className='text-sm self-center'>
           {fileUploadError ? (
             <span className='text-red-700'>
-              Houve um erro! (A imagem deve ter até 2 mb)
+              Houve um erro! (A foto deve ter um tamanho máximo de 2 MB.)
             </span>
           ) : filePerc > 0 && filePerc < 100 ? (
             <span className='text-slate-700'>{`Fazendo o upload ${filePerc}%`}</span>
           ) : filePerc === 100 ? (
-            <span className='text-green-700'>
+            <span className='text-slate-700'>
               Upload de imagem feito com sucesso!
             </span>
           ) : (
@@ -90,23 +122,31 @@ export default function Profile() {
         <input
           type='text'
           placeholder='Nome'
+          defaultValue={currentUser.username}
           id='username'
           className='border p-3 rounded-lg'
+          onChange={handleChange}
         />
         <input
           type='text'
           placeholder='E-mail'
+          defaultValue={currentUser.email}
           id='email'
           className='border p-3 rounded-lg'
+          onChange={handleChange}
         />
         <input
-          type='text'
+          type='password'
           placeholder='Senha'
           id='password'
           className='border p-3 rounded-lg'
+          onChange={handleChange}
         />
-        <button className='w-60 mx-auto bg-emerald-700 rounded-lg text-white p-3 hover:opacity-90 disabled:opacity-80'>
-          Update
+        <button
+          disabled={loading}
+          className='w-60 mx-auto bg-emerald-700 rounded-lg text-white p-3 hover:opacity-90 disabled:opacity-80'
+        >
+          {loading ? 'Carregando...' : 'Atualizar'}
         </button>
       </form>
       <div className='flex justify-between mt-5'>
@@ -117,6 +157,10 @@ export default function Profile() {
           Deslogar
         </span>
       </div>
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+      <p className='text-green-700 mt-5'>
+        {updateSuccess ? 'Usuário atualizado com sucesso!' : ''}
+      </p>
     </div>
   );
 }
