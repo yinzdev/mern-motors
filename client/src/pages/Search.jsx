2e9ee98e1cd6d566b29a9bcedf0d27f9c78e636b
yinzdev <yinzdev@gmail.com>
2024-04-20 00:36:07 -0300

    search onSubmit functionality done

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,8 +1,77 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function Search() {
+  const navigate = useNavigate();
+  const [sidebarData, setSidebarData] = useState({
+    searchTerm: '',
+    sort: 'created_at',
+    order: 'desc',
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [listings, setListings] = useState([]);
+  const [showMore, setShowMore] = useState(false);
+  console.log(listings);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    const sortFromUrl = urlParams.get('sort');
+    const orderFromUrl = urlParams.get('order');
+
+    if (searchTermFromUrl || sortFromUrl || orderFromUrl) {
+      setSidebarData({
+        searchTerm: searchTermFromUrl || '',
+        sort: sortFromUrl || 'created_at',
+        order: orderFromUrl || 'desc',
+      });
+    }
+
+    const fetchListings = async () => {
+      setLoading(true);
+      setShowMore(false);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/api/listing/get?${searchQuery}`);
+      const data = await res.json();
+      if (data.length > 8) {
+        setShowMore(true);
+      } else {
+        setShowMore(false);
+      }
+      setListings(data);
+      setLoading(false);
+    };
+
+    fetchListings();
+  }, [location.search]);
+
+  const handleChange = (e) => {
+    if (e.target.id === 'searchTerm') {
+      setSidebarData({ ...sidebarData, searchTerm: e.target.value });
+    }
+    if (e.target.id === 'sort_order') {
+      const sort = e.target.value.split('_')[0] || 'created_at';
+      const order = e.target.value.split('_')[1] || 'desc';
+
+      setSidebarData({ ...sidebarData, sort, order });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams();
+    urlParams.set('searchTerm', sidebarData.searchTerm);
+    urlParams.set('sort', sidebarData.sort);
+    urlParams.set('order', sidebarData.order);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
     <div className='flex flex-col md:flex-row'>
       <div className='p-7'>
-        <form>
+        <form onSubmit={handleSubmit} className='flex flex-col'>
           <div className='mb-4'>
             <label className='font-semibold mb-2 block'>Pesquisar:</label>
             <input
@@ -10,15 +79,22 @@ export default function Search() {
               id='searchTerm'
               placeholder='Marca, modelo, cidade, ano...'
               className='border rounded-lg p-3 w-full'
+              value={sidebarData.searchTerm}
+              onChange={handleChange}
             />
           </div>
           <div className='flex flex-col gap-2 mb-2'>
             <label className='font-semibold'>Filtrar por:</label>
-            <select id='sort_order' className='border rounded-lg p-3'>
-              <option>Menor preço</option>
-              <option>Maior preço</option>
-              <option>Mais novos</option>
-              <option>Mais antigos</option>
+            <select
+              onChange={handleChange}
+              defaultValue={'created_at_desc'}
+              id='sort_order'
+              className='border rounded-lg p-3'
+            >
+              <option value='price_asc'>Menor preço</option>
+              <option value='price_desc'>Maior preço</option>
+              <option value='createdAt_desc'>Mais novos</option>
+              <option value='createdAt_asc'>Mais antigos</option>
             </select>
           </div>
           <div className='text-center'>
@@ -29,7 +105,7 @@ export default function Search() {
         </form>
       </div>
       <div className='text-center md:text-left'>
-        <h1 className='text-3xl font-semibold border-b p-3 mt-0 md:mt-5'>
+        <h1 className='text-3xl font-semibold border-b p-3 mt-0 md:mt-12'>
           Resultados da busca:
         </h1>
       </div>
